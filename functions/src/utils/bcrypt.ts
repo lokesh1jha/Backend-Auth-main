c1ff564a217290ac078c566c614e7abee0e73494
Lokeshjha <lokesh.jha@leverageedu.com>
2023-08-11 21:40:37 +0530

    bcrypt function defined

--- functions/src/utils/bcrypt.ts
+++ functions/src/utils/bcrypt.ts
@@ -1,5 +1,9 @@
 import bcrypt from 'bcryptjs';
 
-export const comparePassword = async (password: string, hashedPassword: string) => {}; // TODO
+export const comparePassword = async (password: string, hashedPassword: string): Promise<boolean> => {
+    return await bcrypt.compare(password, hashedPassword);
+}; // TODO
 
-export const hashPassword = (password: string) => {}; // TODO
+export const hashPassword = (password: string): Promise<String> => {
+    return bcrypt.hash(password, 5);
+}; // TODO
