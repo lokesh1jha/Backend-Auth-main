e094752753ca814a58f26ca37697112acdce6096
Lokeshjha <lokesh.jha@leverageedu.com>
2023-08-13 08:21:29 +0530

    datatype check fix for user_id

--- functions/src/components/api/account/account.controller.ts
+++ functions/src/components/api/account/account.controller.ts
@@ -7,7 +7,9 @@ import { logger } from 'firebase-functions/v1';
 export const updateAccount = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { name, phone, address } = req.body;
-        await service.updateAccount(req.user.user_id, name, phone, address);
+        let user_id = req.user.user_id;
+        if(!user_id) throw badImplementationException('user_id is not set properly');
+        await service.updateAccount(user_id, name, phone, address);
 
         res.status(200).json({ message: 'OK' });
     } catch (err) {
@@ -19,7 +21,9 @@ export const updateAccount = async (req: Request, res: Response, next: NextFunct
 export const updatePassword = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { password } = req.body;
-        await service.updatePassword(req.user.user_id, password);
+        let user_id = req.user.user_id;
+        if(!user_id) throw badImplementationException('user_id is not set properly');
+        await service.updatePassword(user_id, password);
         res.status(200).json({ message: 'OK' });
     } catch (err) {
         logger.error(err);
